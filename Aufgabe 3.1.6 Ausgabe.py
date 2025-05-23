e9160d5c8a307433a316b02c10f93257e7c91d15
qays <qays@qays.de>
2025-05-23 14:38:58 +0200

    Qays Manssour

--- Aufgabe 3.1.6 Ausgabe.py
+++ Aufgabe 3.1.6 Ausgabe.py
@@ -1,3 +1,4 @@
+# Das ist ein Datensatz.
 
 full_name = "Max Mustermann"
 age = 30
@@ -17,3 +18,26 @@ output_sentense = full_name + " ist " +  str(age) + " Jahre alt, " + gender + ",
 print(output_sentense)
 
 # Max Mustermann ist 30 Jahre alt, männlich, ist 1.8 m groß und wiegt 75 kg. Er hat braune Augen und schwarze Haare. Er arbeitet als Softwareentwickler und mag Lesen gern. Seine Charaktereigenschaft ist freundlich.
+
+
+
+# Erstelle selber eine Datensatz aus deinen Gedanken und erstelle einen Satz als Ausgabe.
+
+full_name = "Qays Manssour"
+age = 38
+gender = "männlich"
+height = 1.76
+weight = 78
+eye_color = "braun"
+hair_color = "schwarz"
+position = "Schüler"
+hobby = "Lesen"
+character_traits = "freundlich"
+
+
+output_sentense = full_name + " ist " +  str(age) + " Jahre alt, " + gender + ", ist " + str(height) + " m groß und wiegt " + str(weight) + " kg. Er hat " +  eye_color + "e Augen und " + hair_color + "e Haare. Er arbeitet als " + position + " und mag " + hobby + " gern. Seine Charaktereigenschaft ist " + character_traits + "."
+
+print(output_sentense)
+
+
+# Qays Manssour ist 38 Jahre alt, männlich, ist 1.76 m groß und wiegt 78 kg. Er hat braune Augen und schwarze Haare. Er arbeitet als Schüler und mag Lesen gern. Seine Charaktereigenschaft ist freundlich.
