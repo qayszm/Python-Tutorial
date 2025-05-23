44d134f2df2c39607c98fc464b25b5dc623d10f8
qays <qays@qays.de>
2025-05-23 11:38:44 +0200

    Variable definiert

---
+++ Aufgabe 3.1.5 Variable.py
@@ -0,0 +1,32 @@
+# Erstelle ein Programm in Python mit 10 Variablen in Anlehnung an ein Fahrzeug.
+vehicle_color = "blau"
+vehicle_size = "groß"
+vehicle_door_count = 3
+fuel_type = "Diesel"
+vehicle_type = "LKW"
+vehicle_brand = "Volvo"
+manufacture_year = 2024
+transmission_type = "manual"
+registration_country = "Deutschland"
+mileage = 132500
+
+#Bringe die Werte in der Konsole zur Ausgabe.
+
+print(vehicle_color)
+print(vehicle_size)
+print(vehicle_door_count)
+print(fuel_type)
+print(vehicle_type)
+print(vehicle_brand)
+print(manufacture_year)
+print(transmission_type)
+print(registration_country)
+print(mileage)
+
+
+# Tausche Werte zwischen zwei Variablen.
+
+vehicle_color, vehicle_size = vehicle_size, vehicle_color
+
+print(vehicle_color)
+print(vehicle_size)
