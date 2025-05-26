241c4ddae456df413a9c4398cf76fa7294135787
qays <qays@qays.de>
2025-05-26 15:10:31 +0200

    elif und else

--- Die if Anweisung.py
+++ Die if Anweisung.py
@@ -7,8 +7,19 @@ age = int(input("Geben Sie Ihr Alter ein: "))
 if age < 18:
     print("Achtung, der Nutzer minderjährig.")
     print("ich bin immer noch innerhalb if-Anweisung / if-Block.")
+elif age == 18:
+    print("Der Nutzer ist genau 18 Jahre alt.") 
+elif age == 19:
+    print("Der Nutzer ist genau 19 Jahre alt.") 
+else:
+    print("Der Nutzer ist volljährig.")
 
 print("ich bin außererhalb if-Anweisung / if-Block. _ Neue Zeile / Code wird immer ausgeführt.")
 
 # Bedingung ist True, Wenn das Alter kleiner als 18 ist
-# der Code innerhalb des if-Blocks (nach Rückung / TAB) wird ausgeführt, wenn die Bedingung wahr ist.
+# der Code innerhalb des if-Blocks (nach Rückung / TAB) wird ausgeführt, wenn die Bedingung wahr / True ist.
+# Wenn die Bedingung ( von if ) nicht wahr / False ist, wird die elif-Anweisung überprüft.
+# ( mehrere Bedingung hinzufügen ) Wenn die elif-Bedingung wahr / True ist, wird der Code innerhalb des elif-Blocks ausgeführt.
+# Wenn alle Bedingungen (if , elif) nicht wahr / False sind, wird der Code innerhalb des else-Block (nach Rückung / TAB) ausgeführt.
+
+# Der Code außerhalb der if-Anweisung wird immer ausgeführt, unabhängig von der Bedingung.
