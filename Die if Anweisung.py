d8d3eeb5c612496388b6df6ba07f7e92be3d4242
qays <qays@qays.de>
2025-05-26 14:24:57 +0200

    if Anweisung

---
+++ Die if Anweisung.py
@@ -0,0 +1,14 @@
+
+# ---------------- Die if Anweisung ----------------
+# Die if-Anweisung wird verwendet, um  zu überprüfen und Code nur auszuführen, wenn die Bedingung wahr ist.
+
+age = int(input("Geben Sie Ihr Alter ein: "))
+
+if age < 18:
+    print("Achtung, der Nutzer minderjährig.")
+    print("ich bin immer noch innerhalb if-Anweisung / if-Block.")
+
+print("ich bin außererhalb if-Anweisung / if-Block. _ Neue Zeile / Code wird immer ausgeführt.")
+
+# Bedingung ist True, Wenn das Alter kleiner als 18 ist
+# der Code innerhalb des if-Blocks (nach Rückung / TAB) wird ausgeführt, wenn die Bedingung wahr ist.
