a55bd03ab5d2ba0895868b64a0102b5b7e6e5903
qays <qays@qays.de>
2025-05-26 13:30:24 +0200

    Zuweisungsoperator

---
+++ Zuweisungsoperator.py
@@ -0,0 +1,28 @@
+
+# ------------------ Der Zuweisungsoperator (=) ------------------
+
+# Der Zuweisungsoperator (=) wird verwendet, um einer Variablen einen Wert zuzuweisen.
+# Die rechte Seite des Zuweisungsoperators wird ausgewertet und das Ergebnis wird der Variablen auf der linken Seite zugewiesen.
+
+example1 = (22-2) * 3
+print("Beispiel 1: ", example1)  # Ausgabe: 60
+
+#-------------------------------------------------
+
+Value1 = 5
+Value2 = 5
+
+Value1 = Value1 + 10
+
+print(Value1)  # Ausgabe: 15
+
+#------------------------- ( += , -= , *= , /= )------------------------
+
+Value2 += 10  # Dies ist eine Kurzschreibweise für Value2 = Value2 + 10
+print(Value2)  # Ausgabe: 15    
+
+Value2 /= 10 
+print(Value2)  # Ausgabe: 1.5   # Kurzschreibweise für (Die neue value2 = 15) Value2 = Value2 / 10
+
+
+
